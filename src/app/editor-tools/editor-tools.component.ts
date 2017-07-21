0b819246bfd2ee52eda3265d360ae4c5bda07baf
unknown <Ashish.Mishra@BOLD-AshishM.LCINDIA.LOCAL>
2017-07-21 17:29:54 +0530

    functions

--- src/app/editor-tools/editor-tools.component.ts
+++ src/app/editor-tools/editor-tools.component.ts
@@ -1,34 +1,77 @@
-import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
-
+import { Component, OnInit, Renderer, ElementRef, HostListener, Input, Output, EventEmitter} from '@angular/core';
+import {EditorToolConfiguration} from './editor-tools.configuration';
 
 @Component({
   selector: 'editor-tools',
   templateUrl: './editor-tools.component.html',
-  styleUrls: ['./editor-tools.component.css']
+  styleUrls: ['./editor-tools.component.css']  
 })
 
-export class EditorToolsComponent implements OnInit {
+export class EditorToolsComponent implements OnInit { 
 
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
+  private componentElement: any;
+  //@Input() editorContent : string;
+  //@Output() notify: EventEmitter<string> = new EventEmitter<string>();
+  
+
+
+  constructor ( private elementRef: ElementRef, //Element Class to get the exact emelent this component rendered upon
+                private renderer: Renderer, // To Be used to re-render the DOM after tweaks
+                public toolsConfiguration: EditorToolConfiguration // Components class to set what components can be shown or hidden on the UI                
+              )
+  {      
+      this.componentElement = renderer.selectRootElement(this); //will have component root element     
   }
 
-  private componentElement: any;  
-  
-  constructor(private elementRef: ElementRef, private renderer: Renderer2){
-      //el = elementRef.nativeElement;
-      this.componentElement = renderer.selectRootElement(this); //will have component root element
+  ngOnInit(){}
+   @HostListener('click') click() {
+      let contentElement = this.elementRef.nativeElement.querySelector("DIV.editableContent");
+      
+      let undoButton = contentElement.previousElementSibling.querySelector("app-editor-tool-undo button");      
+
+      if(contentElement && contentElement.contentEditable != "true")
+      {        
+        let activeComponents = this.elementRef.nativeElement.parentElement.querySelectorAll("DIV.editableContent[contenteditable='true']");
+        
+        if(activeComponents && activeComponents.length > 0)
+        {
+          let loopLength = activeComponents.length;
+          for (var index = 0; index < loopLength; index++) {
+            var currentElement = activeComponents[index];
+            currentElement.contentEditable = false;
+            this.renderer.setElementClass(currentElement.previousElementSibling, "dn", true);
+          }  
+        }
+
+
+        contentElement.contentEditable = true;
+        contentElement.focus();
+        this.renderer.setElementClass(contentElement.previousElementSibling, "dn", false);
+      }     
+      //else if(undoButton && (undoButton.disabled == true || undoButton.disabled == "disabled"))
+      //{
+        //this.renderer.setElementAttribute(undoButton, "disabled", null);
+        //this.renderer.setElementAttribute(contentElement.previousElementSibling.querySelector("app-editor-tool-redo button"), "disabled", null);        
+      //}
+
+      //this.notify.emit("this.editorContent");
+  }
+
+   @HostListener('keyup', ['$event']) keyup(e) {    
+            
+      let contentElement = this.elementRef.nativeElement.querySelector("DIV.editableContent");      
+      let undoButton = contentElement.previousElementSibling.querySelector("app-editor-tool-undo button");
+
+      if(undoButton && (undoButton.disabled == true || undoButton.disabled == "disabled"))
+      {
+        this.renderer.setElementAttribute(undoButton, "disabled", null);
+        this.renderer.setElementAttribute(contentElement.previousElementSibling.querySelector("app-editor-tool-redo button"), "disabled", null);        
+      }
    }
-  
 
-  Boldify(isBold){
-    debugger;
-    if(isBold){
-      isBold = false;
-    }else{
-      isBold = true;
-    }
-    //this.elementRef.nativeElement.parentElement.children[1].setAttribute("ng-reflect-app-make-bold",true);
-    //console.log("Boldified"+ this.componentElement);
-  }
+  // Needs Little more modification for further use
+  focusOutFunction(evt) {    
+      //     this.elementRef.nativeElement.querySelector("DIV.editableContent").contentEditable = false;     
+      //     this.isEditing = false;      
+  }  
 }
--- src/app/ui-editor/ui-editor.component.ts
+++ src/app/ui-editor/ui-editor.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ElementRef, Renderer} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { EditorToolsComponent } from '../editor-tools/editor-tools.component';
+import { EditorToolConfiguration } from '../editor-tools/editor-tools.configuration';
 
 @Component({
   selector: 'app-ui-editor',  
   templateUrl: './ui-editor.component.html',
-  styleUrls: ['./ui-editor.component.css']  
+  styleUrls: ['./ui-editor.component.css']    
 })
-export class UiEditorComponent {
-  
-  isBold: boolean = true;
+export class UiEditorComponent implements OnInit {
 
-  constructor() { }
+  public editorContent: string = "ui-editor works!"; // Web content to be shown on the html page
 
+  public editorContentTemp: string = "Yes Temporary ui-editor works!";
+
+  constructor(private elementRef: ElementRef, //Element Class to get the exact emelent this component rendered upon
+                private renderer: Renderer, // To Be used to re-render the DOM after tweaks
+                private toolsConfiguration: EditorToolConfiguration) //Setting the configuration to show or hide editor controls on the UI through tools
+  { 
+    toolsConfiguration.isBold = true;   
+    toolsConfiguration.isItalic = true;    
+    toolsConfiguration.isUnderline = true;
+    toolsConfiguration.isBullet = true;
+    toolsConfiguration.isUndo = true;
+    toolsConfiguration.isRedo = true;
+  }
+
+   ngOnInit(){
+     console.log(this.editorContent);
+   }
+
+   saveData()
+   {
+      console.log(this.editorContent);      
+   }  
+
+   addContent(){
+     var activeComponent = this.elementRef.nativeElement.parentElement.querySelector("DIV.editableContent[contenteditable='true']");
+     activeComponent.innerHTML = activeComponent.innerHTML + "<b>Test Me</b>";
+     debugger;
+   }
 }
